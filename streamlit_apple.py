de45e2d5813f2a35ade024b01fe00dd319dca4fc
rsultanov-ds <soultanoff@gmail.com>
2023-05-31 14:45:55 +0300

    changes made to the appearance of the app

--- streamlit_apple.py
+++ streamlit_apple.py
@@ -4,8 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 
-st.write('<div align="center"><h3><b>Apple: объём торгов и стоимость акций за 10 лет</b></h3></div>', unsafe_allow_html=True)
+st.write('<div align="center"><h3><b>Apple: стоимость акций и объём торгов за 10 лет</b></h3></div>', unsafe_allow_html=True)
 
+st.sidebar.header('О приложении')
+st.sidebar.info('В данном приложении демонстрируется объём торгов и стоимость акций компании Apple за последние 10 лет')
+
+if st.sidebar.checkbox('Желаю ознакомиться со скрытым содержанием приложения'):
+    st.sidebar.info('Данное предложение является скрытым содержанием приложения. Благодарим за интерес!')
 
 apple = yf.Ticker('AAPL')
 appledf = apple.history(period='10y', interval = '1d')
@@ -17,16 +22,22 @@ min_stock_date = appledf.loc[appledf['Close'].idxmin(), 'Date'].date()
 
 chart_width = 1000
 
+''
+''
+st.write('#### Динамика стоимости акций')
 plt.figure(figsize=(chart_width / 100, 6))
 plt.plot(appledf['Date'], appledf['Close'])
 plt.xlabel('Год')
 plt.ylabel('Цена акции (долл. США) на закрытии торгов')
 st.pyplot(plt)
 
-if st.checkbox('Показать максимальные и минимальные значения стоимости акций **Apple** за 10 лет'):
-    st.write('Максимальное значение: {:0.2f} долл. США, дата: {}'.format(max_stock, max_stock_date))
-    st.write('Минимальное значение: {:0.2f} долл. США,  дата: {}'.format(min_stock, min_stock_date))
+st.write('**Максимальные и минимальные значения стоимости акций **Apple** за 10 лет:**')
+st.write('Максимальное значение: {:0.2f} долл. США, дата: {}'.format(max_stock, max_stock_date))
+st.write('Минимальное значение: {:0.2f} долл. США,  дата: {}'.format(min_stock, min_stock_date))
 
+''
+''
+st.write('#### Динамика объёма торгов')
 plt.figure(figsize=(chart_width / 100, 6))
 plt.plot(appledf['Date'], appledf['Volume'])
 plt.xlabel('Год')
